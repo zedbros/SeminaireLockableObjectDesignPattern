62ec6343a04b6520552ef48f2ebf308e518ecb75
Sebastian0211-vs <sebastian.morsch@edu.vs.ch>
2026-01-09 19:25:56 +0100

    Refactor Cars class; improve null checks and update car wash logic

--- src/seminaireLockableObject/Cars.java
+++ src/seminaireLockableObject/Cars.java
@@ -1,17 +1,17 @@
 package seminaireLockableObject;
 
-public class Cars implements Runnable{
+public class Cars implements Runnable {
     private final Car car;
     private final Lockable target;
 
-    public Cars(@NonNull Car car, @NonNull Lockable target) {
-        this.car = car;
-        this.target = target;
+    public Cars(Car car, Lockable target) {
+        this.car = java.util.Objects.requireNonNull(car, "car");
+        this.target = java.util.Objects.requireNonNull(target, "target");
     }
 
     @Override
     public void run() {
-        if (car.wash(target)) {
+        if (!car.wash(target)) {
             useCarWash(car, target.getLocker(), target);
         } else {
             System.out.println(car.getName() + " has started his car wash.");
@@ -20,11 +20,12 @@ public class Cars implements Runnable{
 
     public void useCarWash(Car car, Car locker, Lockable target) {
         System.out.println(car.getName() + " is waiting for the car wash occupied by " + locker.getName());
-        while (car.wash(target)) {
+        while (!car.wash(target)) {
             try {
-                Thread.sleep(500);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                Thread.currentThread().interrupt();
+                return;
             }
         }
         System.out.println(car.getName() + " has started his car wash.");
