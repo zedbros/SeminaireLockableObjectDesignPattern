74679f5ab68cf870c61f244a1272f817c6be8410
Sebastian0211-vs <sebastian.morsch@edu.vs.ch>
2026-01-09 19:03:53 +0100

    Enhance Car class; make fields final, add random model and color generation, and update getName method format

--- src/seminaireLockableObject/Car.java
+++ src/seminaireLockableObject/Car.java
@@ -1,13 +1,22 @@
 package seminaireLockableObject;
 
+import java.util.List;
+
 public class Car {
 
-    private int id;
-    private String model;
-    private String color;
+    private final int id;
+    private final String model;
+
+    public Car() {
+        this.id = (int) (Math.random() * 1000);
+        List<String> models = List.of("Sedan", "SUV", "Coupe", "Convertible", "Hatchback", "Van", "Truck", "Wagon", "Sports Car", "Diesel");
+        this.model = models.get((int) (Math.random() * models.size()));
+        List<String> colors = List.of("Red", "Blue", "Green", "Black", "White");
+        String color = colors.get((int) (Math.random() * colors.size()));
+    }
 
     public Object getName() {
-        return model + " " + id;
+        return model + " #" + id;
     }
     public boolean wash(Lockable target) {
         return target.wash(this);
